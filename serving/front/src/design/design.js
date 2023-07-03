6522a7924aed24b6dcdf11b2b3d7a609bf536576
JunOnJuly <wlgnstjd0717@gmail.com>
2023-07-03 17:49:30 +0900

    feature image send

---
+++ serving/front/src/design/design.js
@@ -0,0 +1,13 @@
+export const colors = {
+  MainBlue: '#152554',
+  WhiteBlue: '#A9B2CD',
+  WhiterBlue: '#7683AA',
+  DarkerBlue: '#505F8F',
+  DarkBlue: '#152554',
+
+  MainWood: '#D49D6A',
+  WhiteWood: '#FFE5CC',
+  WhiterWood: '#FCCFA5',
+  DarkerWood: '#AA6F39',
+  DarkWood: '#7D4614'
+}
