21f76b6154cd4787b88beddf3084f84158715dfe
JunOnJuly <wlgnstjd0717@gmail.com>
2023-07-19 19:19:21 +0900

    update encode

--- serving/back/images/views.py
+++ serving/back/images/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import requests
 from PIL import Image
+import base64
 
 @api_view(['GET', 'POST'])
 def process(request):
@@ -18,9 +19,12 @@ def process(request):
     
     print(request.FILES['img'])
     
+    with open(request.FILES['img'], 'rb') as f: 
+        base64_img = base64.b64encode(f.read())
+
     ai_url = 'http://118.67.131.164:40002/images/process/'
     ai_img = {
-        'img' : request.FILES['img']
+        'img' : base64_img
     }
 
     res = requests.post(ai_url, data=ai_img)
