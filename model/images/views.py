f9d5b4d2b8297e1e039e94c575cf3a77ad56bd49
JunOnJuly <wlgnstjd0717@gmail.com>
2023-07-19 18:05:06 +0900

    fix views

--- model/images/views.py
+++ model/images/views.py
@@ -2,11 +2,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from PIL import Image
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def process(request):
-    """Temp
+    """Get Imaage from main backend and Send PCD to main backend
+
+    Args:
+        request: Request from main backend [png, jpg]
+    Returns:
+        temp -> messages
     """
 
-    if request.method == 'GET':
-        return Response({"message": "this is GET"})
     return Response({"message": "this is POST"})
