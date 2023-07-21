7f6d6b41377c08091740621723c9b0df29648cd0
JunOnJuly <wlgnstjd0717@gmail.com>
2023-07-21 17:22:53 +0900

    front build

--- serving/front/src/pages/image/ShowPcd.js
+++ serving/front/src/pages/image/ShowPcd.js
@@ -3,17 +3,17 @@ import { OrbitControls } from '@react-three/drei'
 import { PCDLoader } from 'three-stdlib'
 import * as s from "./ShowPcd_css";
 
-function Points(props) {
+function Points() {
   const points = useLoader(PCDLoader, 'img_dir/temp_pcd.pcd')
   return <primitive object={points} {...props} />
 }
 
-export default function ShowPcd(props) {
+export default function ShowPcd() {
   return (
     <s.ShowPcdBackground>
       <Canvas
       camera={{ position: [3, 2, 0]}}>
-        <Points pcd={props.pcd} rotation={[Math.PI/2, Math.PI, Math.PI/2]} material-size={0.001} material-color="white" />
+        <Points rotation={[Math.PI/2, Math.PI, Math.PI/2]} material-size={0.001} material-color="white" />
         <OrbitControls autoRotate={true}/>
       </Canvas>
     </s.ShowPcdBackground>
