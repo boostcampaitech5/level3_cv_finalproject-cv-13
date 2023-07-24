af5dba647231861011286493292760f8e4a731a1
JunOnJuly <wlgnstjd0717@gmail.com>
2023-07-24 16:58:38 +0900

    fix text position

--- serving/front/src/pages/image/showPcd/ShowPcd.js
+++ serving/front/src/pages/image/showPcd/ShowPcd.js
@@ -26,9 +26,18 @@ export default function ShowPcd(props) {
   return (
     <s.ShowPcdBackground>
       <s.PointSizeDiv>
-        <s.TopPointSizeText>
-          Dot Size Control
-        </s.TopPointSizeText>
+        <s.PointSizeTextDiv>
+          <s.TopPointSizeText>
+            Dot
+          </s.TopPointSizeText>
+          <s.TopPointSizeText>
+            Size
+          </s.TopPointSizeText>
+          <s.TopPointSizeText>
+            Control
+          </s.TopPointSizeText>
+        </s.PointSizeTextDiv>
+        
         <s.PointSizeUpBtn onClick={() => sizeUp()}>
           <s.PointSizeText>
             +
